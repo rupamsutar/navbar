7a25265bc0e877ac796a1b1130182bece75ba78c
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-09 07:41:51 +0530

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -8,31 +8,31 @@ const Navbar = () => {
 
   return (
     <nav>
-        <div className="nav-center">
-            <div className="nav-header">
-                <img src={logo} alt="logo" className='logo'/>
-                <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
-                    <FaBars />
-                </button>
-            </div>
-            {showLinks && (
-                <div className="links-container">
-                <ul className="links">
-                    {links.map((link) => {
-                        const {id, url, text} = link;
-                        return (
-                            <li key={id}>
-                                <a href={url}>{text}</a>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-            )}
-            
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="logo" />
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
         </div>
+        <div className={showLinks ? 'links-container show-container' : 'links-container'}>
+          <ul className="links">
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
